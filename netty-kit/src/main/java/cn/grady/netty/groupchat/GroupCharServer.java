3f68dae55b1bb7521f8f2722d97da0c99bcde2be
gradyroc <13802504607@163.com>
2021-06-15 02:15:43 +0800

    服务端 handler fix

--- netty-kit/src/main/java/cn/grady/netty/groupchat/GroupCharServer.java
+++ netty-kit/src/main/java/cn/grady/netty/groupchat/GroupCharServer.java
@@ -22,6 +22,7 @@ public class GroupCharServer {
 
     /**
      * run 方法,处理客户端请求
+     *
      * @param
      */
     public void run() throws InterruptedException {
@@ -30,39 +31,36 @@ public class GroupCharServer {
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
 
-           ServerBootstrap serverBootstrap = new ServerBootstrap();
-           serverBootstrap.group(bossGroup,workerGroup)
-                   .channel(NioServerSocketChannel.class)
-                   .option(ChannelOption.SO_BACKLOG,128)
-                   .childOption(ChannelOption.SO_KEEPALIVE,true)
-                   .childHandler(new ChannelInitializer<SocketChannel>() {
-                       @Override
-                       protected void initChannel(SocketChannel ch) throws Exception {
-                           //先获取pipline
-                           ChannelPipeline pipeline = ch.pipeline();
-                           //向pipeline中加入解码器
-                           pipeline.addLast("decoder",new StringDecoder());
-                           //向pipeline中加入编码器
-                           pipeline.addLast("encoder" ,new StringEncoder());
-                           // self define
-                           pipeline.addLast(null);
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //先获取pipline
+                            ChannelPipeline pipeline = ch.pipeline();
+                            //向pipeline中加入解码器
+                            pipeline.addLast("decoder", new StringDecoder());
+                            //向pipeline中加入编码器
+                            pipeline.addLast("encoder", new StringEncoder());
+                            // self define
+                            pipeline.addLast("myGroupChatServerHandler", new GroupChatServerHandler());
 
 
-                       }
-                   });
-           System.out.println("netty server started");
-           ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+                        }
+                    });
+            System.out.println("netty server started");
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
 
-           //监听关闭事件
-           channelFuture.channel().closeFuture().sync();
+            //监听关闭事件
+            channelFuture.channel().closeFuture().sync();
 
-       }finally {
-           bossGroup.shutdownGracefully();
-           workerGroup.shutdownGracefully();
-       }
-
-
-
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
 
 
     }
@@ -72,8 +70,6 @@ public class GroupCharServer {
         new GroupCharServer(7000).run();
 
     }
-
-
 
 
 }
