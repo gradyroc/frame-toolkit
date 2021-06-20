b8af2750f7e65887605f34bdb38b0cde68a4a1d3
gradyroc <13802504607@163.com>
2021-06-21 02:33:10 +0800

    1、client 端编码器的接受数据类型和真正发送的数据类型之间的关系 2、发送的数据和编码器接受的类型不一致分析

--- netty-kit/src/main/java/cn/grady/netty/inoutboundhandler/MyClientHandler.java
+++ netty-kit/src/main/java/cn/grady/netty/inoutboundhandler/MyClientHandler.java
@@ -22,9 +22,31 @@ public class MyClientHandler extends SimpleChannelInboundHandler<Long> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
 
-        System.out.println("client send message method  is called !");
-        ctx.writeAndFlush(123456L);//发送的是long
+//        System.out.println("client send message method  is called !");
+//        ctx.writeAndFlush(123456L);//发送的是long
 
+        /**
+         * 1、abcdabcdabcdabcd 16个字节
+         * 2、该处理器的前一个handler 是 MyLongToByteEncoder
+         * 3、 MyLongToByteEncoder 的父类 是 MessageToByteEncoder.write()
+         *
+         * if (acceptOutboundMessage(msg)) { //判断当前msg是不是应该处理的类型，如果是，则编码，不是，则直接发送数据
+         *                 @SuppressWarnings("unchecked")
+         *                 I cast = (I) msg;
+         *                 buf = allocateBuffer(ctx, cast, preferDirect);
+         *                 try {
+         *                     encode(ctx, cast, buf);
+         *                 } finally {
+         *                     ReferenceCountUtil.release(cast);
+         *                 }
+         *             } else {
+         *             //直接写数据
+         *                 ctx.write(msg, promise);
+         *             }
+         *
+         *  4、因此我们在编写encoder 时，要注意传入的类型和处理的数据类型要一致
+         */
+        ctx.writeAndFlush(Unpooled.copiedBuffer("abcdabcdabcdabcd",CharsetUtil.UTF_8));
 
     }
 }
