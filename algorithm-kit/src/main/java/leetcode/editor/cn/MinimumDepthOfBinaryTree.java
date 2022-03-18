6f9a04d27d7dd2077542c82917af0c10c6cd06ef
gradyroc <13802504607@163.com>
2022-03-18 11:55:52 +0800

    111-minimal depth of binary tree

---
+++ algorithm-kit/src/main/java/leetcode/editor/cn/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,95 @@
+package leetcode.editor.cn;
+
+//Given a binary tree, find its minimum depth. 
+//
+// The minimum depth is the number of nodes along the shortest path from the roo
+//t node down to the nearest leaf node. 
+//
+// Note: A leaf is a node with no children. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 2
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [2,null,3,null,4,null,5,null,6]
+//Output: 5
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 105]. 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 693 👎 0
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MinimumDepthOfBinaryTree{
+    public static void main(String[] args) {
+        Solution solution = new MinimumDepthOfBinaryTree().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        // root 本身就是一层，depth 初始化为 1
+        int depth = 1;
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            for (int i =0;i<size;i++){
+                TreeNode cur = queue.poll();
+                //判断是否到达重点
+                if (cur.left==null &&cur.right==null){
+                    return depth;
+                }
+                // 将 cur 的相邻节点加入队列
+                if (cur.left != null){
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right !=null){
+                    queue.offer(cur.right);
+                }
+
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ algorithm-kit/src/main/java/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+/**
+ * @author grady
+ * @version 1.0, on 11:41 2022/3/18.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
