612793f6b6239199823425f641151018d7a14fc9
gradyroc <13802504607@163.com>
2023-04-30 00:37:18 +0800

    upup

--- springboot-kit/src/main/java/cn/grady/BootStrap.java
+++ springboot-kit/src/main/java/cn/grady/BootStrap.java
@@ -1,7 +1,11 @@
 package cn.grady;
 
+import cn.grady.config.AppConfig;
+import cn.grady.config.ConfigInDb;
 import cn.grady.log.ShutdownHook;
+import cn.grady.util.Startable;
 import com.google.common.collect.Lists;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.support.AopUtils;
@@ -16,14 +20,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.SmartLifecycle;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import scala.App;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 import static java.lang.Thread.sleep;
 
@@ -33,6 +41,10 @@ import static java.lang.Thread.sleep;
  */
 @SpringBootApplication
 @Configuration
+@ComponentScan()
+@MapperScan()
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class BootStrap {
     private static final Logger logger = LoggerFactory.getLogger(BootStrap.class);
@@ -80,7 +92,8 @@ public class BootStrap {
     @Order(Integer.MAX_VALUE)
     public static class Hook implements SmartLifecycle,ApplicationContextAware{
 
-        private static boolean isRunning = false;
+        private static AtomicBoolean isRunning = new AtomicBoolean(false);
+        private static List<Startable> startables=null;
 
         /**
          * 2.实现ApplicationContextAware接口
@@ -98,22 +111,75 @@ public class BootStrap {
             logger.info("print beans in container");
 
             printBeans();
+            
+            setStartables();
+            for (Startable startable : startables) {
+                startable.startup();
+            }
 
-            isRunning = true;
+            AppContext.getCtx().registerShutdownHook();
+
+            //
+            AppConfig appConfig = AppContext.getCtx().getBean(AppConfig.class);
+
+            initialStaticClass();
+            logger.info(String.format("SUCCESS-SIGN: start subSystem success,systemId:{}",appConfig.getSystemId()));
+            isRunning.set(true);
+        }
+
+        private void initialStaticClass() {
+            new ConfigInDb();
+        }
+
+        private void setStartables() {
+            Map<String, Startable> map = AppContext.getCtx().getBeansOfType(Startable.class);
+            Set<String> keys = map.keySet();
+            List<Startable> list = new ArrayList<>();
+            for (String key : keys) {
+                list.add(map.get(key));
+            }
+            Collections.sort(list, new Comparator<Startable>() {
+                @Override
+                public int compare(Startable o1, Startable o2) {
+                    if (o1.getOrder()==o2.getOrder()){
+                        return 0;
+                    }
+                    return o1.getOrder()<o2.getOrder() ? -1:1;
+                }
+            });
+
+            startables = list;
         }
 
         @Override
         public void stop() {
-            logger.info("stop the system");
+
+        }
+
+        @Override
+        public void stop(Runnable callback) {
+            for (Startable startable : startables) {
+                startable.shutdown();
+            }
+
+            SmartLifecycle.super.stop(callback);
+            logger.info("stop the system done !!");
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            isRunning.set(false);
         }
 
         @Override
         public boolean isRunning() {
-            return isRunning;
+            return isRunning.get();
         }
 
         public static boolean isSystemRunning(){
-            return isRunning;
+            return isRunning.get();
         }
         @Override
         public boolean isAutoStartup(){
